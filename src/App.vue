2ec06bf249a5a589fda0b3e72394d00345a701a6
methodispost <597463474@qq.com>
2017-07-25 17:24:51 +0800

    add 2.00

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
   <div id="app">
     <!--头部音乐信息-->
-    <div :style="{backgroundColor: skinColor}" class="header"></div>
+    <div :style="{backgroundColor: skinColor}" class="header">
+      <div @click="isShowAdd=!isShowAdd" class="add-icon">
+        <img src="http://omratag7g.bkt.clouddn.com/add.png" class="icon" alt="methodispost">
+      </div>
+      <div class="music-title">
+        <p>{{ musicTitle }}</p>
+      </div>
+      <div class="list-icon">
+        <img @click="isShowList=!isShowList" src="http://omratag7g.bkt.clouddn.com/Category.png" class="icon" alt="methodispost">
+      </div>
+    </div>
+
+    <!--音乐内容主体区域-->
+    <div @click.stop.prevent="isShowList=false" class="content">
+      <audio :src="musicSrc" preload>
+        Your browser does not support the audio element.
+      </audio>
+
+      <!--添加音乐-->
+    </div>
   </div>
 </template>
 
@@ -16,14 +35,31 @@
     name: 'app',
     //页面载入初始化
     mounted() {
-
+      this.DOM = {
+        audio: document.querySelector('audio'),
+        rotateImg: document.querySelector('.rotateImg')
+      };
+      this.musicSrc = this.musics[this.index].src;
+      this.musicTitle = this.musics[this.index].name;
+//      this.musicImgSrc = this.musics[this.index].musicImgSrc || this.musicSrcDefault;
+//      this.DOM.audio.addEventListener('ended', () => {this.toChange('next').then(this.toAnimate);});
+      Store.fetch('musics').length === 0 ? Store.save(this.musics) : null;
     },
     data() {
       return {
-        skinColor: '#B72712'
+        musics: Store.fetch('musics').length ? Store.fetch('musics') : Object.assign([], MusicData),
+        index: 0,
+        musicTitle: '',
+        skinColor: '#B72712',
+        isShowAdd: false,
+        isShowList: false,
+        musicSrc: '',
+        Dom: {}
       }
     }
   }
+//  console.log(MusicData)
+//  console.log(Object.assign([], MusicData))
 </script>
 
 <style>
@@ -39,5 +75,44 @@
     background-color: #212121;
     display: flex;
     align-items: center;
+  }
+
+  .header .add-icon {
+    width: 30px;
+    height: 30px;
+    flex: 1;
+    text-align: left;
+    padding-left: 10px;
+    padding-top: 5px;
+    cursor: pointer;
+  }
+
+  .header .music-title {
+    flex: 5;
+    text-align: center;
+  }
+
+  .header .music-title p {
+    width: 250px;
+    color: white;
+    text-align: center;
+    white-space: normal;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin: auto;
+  }
+
+  .header .list-icon {
+    width: 30px;
+    height: 30px;
+    flex: 1;
+    text-align: right;
+    padding-right: 10px;
+    padding-top: 5px;
+  }
+
+  .header .list-icon img {
+    width: 23px;
+    cursor: pointer;
   }
 </style>
